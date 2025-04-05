0d9bf61725e8b5b794e68024b4b4e5f1d3e4d4ca
翠花 <git@nange.cn>
2025-04-05 16:05:12 +0800

    Update.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,6 +34,37 @@
 				当前有服务器宕机，请注意
 			</n-alert>
 		</div>
+		<div class="col-span-12 flex flex-col gap-2 overflow-hidden md:col-span-8">
+
+			<div
+				class="border border-gray-200 rounded-lg bg-white px-6 shadow dark:border-gray-700 dark:bg-gray-800"
+			>
+			</div>
+			<div
+				v-show="uptime_loading"
+				class="border border-gray-200 rounded-lg bg-white px-6 shadow dark:border-gray-700 dark:bg-gray-800"
+			>
+				<n-spin class="min-h-40 w-full"> </n-spin>
+			</div>
+			<n-tabs type="segment" animated class="overflow-hidden rounded-lg">
+					<div
+						v-for="(topItem, i) in uptime_data.monitors"
+						:key="i"
+						class="w-full"
+					>
+						<div>{{ i }}</div>
+						<div
+							class="mt-2 border border-gray-200 rounded-lg bg-white px-6 shadow dark:border-gray-700 dark:bg-gray-800"
+						>
+							<div class="w-full divide-y divide-dashed">
+								<ul v-for="(item, j) in topItem" :key="j" class="w-full">
+									<StatusItem :rtl="info_data?.rtl" :data="item"></StatusItem>
+								</ul>
+							</div>
+						</div>
+					</div>
+			</n-tabs>
+		</div>
 		<div class="col-span-12 flex flex-col gap-2 overflow-hidden md:col-span-4">
 			<AboutMe
 				:loading="info_loading"
@@ -70,6 +101,7 @@
 					</n-timeline-item>
 				</n-timeline>
 			</div>
+
 		</div>
 	</div>
 </template>
